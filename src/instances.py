0d5b9acab346ae1b2109b544645a6811f1f72168
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-03-23 23:15:01 +0100

    Implemented logic for listing and providing information about instances

--- src/instances.py
+++ src/instances.py
@@ -15,3 +15,49 @@ def create_instance(
     count: int = typer.Option(1, help="Number of instances to launch"),
 ) -> None: 
     client = boto3.client('ec2')
+    params = {
+        "ImageId": ami,
+        "InstanceType": instance_type,
+        "MinCount": count,
+        "MaxCount": count,
+    }
+    if key_name:
+        params["KeyName"] = key_name
+    if security_group_id:
+        params["SecurityGroupIds"] = [security_group_id]
+
+    response = client.run_instances(**params)
+
+    # Tag instances with Name if provided
+    if name:
+        instance_ids = [inst["InstanceId"] for inst in response["Instances"]]
+        client.create_tags(
+            Resources=instance_ids,
+            Tags=[{"Key": "Name", "Value": name}]
+        )
+
+    # Display results
+    for inst in response["Instances"]:
+        typer.echo(f"Launched: {inst['InstanceId']} ({inst['InstanceType']})")
+
+@instances_app.command("list")
+def list_instances(
+    state: Optional[str] = typer.Option(None, help="Filter by state: running, stopped, etc."),
+) -> None: 
+    ec2 = boto3.resource('ec2')
+    if state:
+        filters = [{"Name": "instance-state-name", "Values": [state]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    
+    for instance in instances:
+        name_tag = ""
+        if instance.tags:
+            name_tag = next((t["Value"] for t in instance.tags if t["Key"] == "Name"), "")
+        typer.echo(
+            f"{instance.id}  {instance.instance_type}  {instance.state['Name']}  "
+            f"{instance.public_ip_address or '-'}  {instance.private_ip_address or '-'}  {name_tag}"
+        )
+
+
