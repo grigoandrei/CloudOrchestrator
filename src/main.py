cb160b28e44e1cfd7e4d3c4fb20da4be1bd34c29
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-04-03 23:39:54 +0200

    Wire CLI entry point and global options

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import typer
+import boto3
+from typing import Optional
+
+from src.instances import instances_app
+from src.security_groups import sg_app
+from src.volumes import volumes_app
+from src.tags import tags_app
+from src.monitor import monitor_app
+from src.cost import cost_app
+
+app = typer.Typer(help="CloudOrchestrator - AWS EC2 management CLI")
+
+app.add_typer(instances_app, name="instances", help="Manage EC2 instances")
+app.add_typer(sg_app, name="sg", help="Manage security groups")
+app.add_typer(volumes_app, name="volumes", help="Manage EBS volumes")
+app.add_typer(tags_app, name="tags", help="Manage resource tags")
+app.add_typer(monitor_app, name="monitor", help="Monitor and stop idle instances")
+app.add_typer(cost_app, name="cost", help="Generate cost reports")
+
+@app.callback()
+def main(
+    region: Optional[str] = typer.Option(None, "--region", help="AWS region"),
+    profile: Optional[str] = typer.Option(None, "--profile", help="AWS profile"),
+):
+    if region or profile:
+        boto3.setup_default_session(region_name=region, profile_name=profile)
+
+if __name__ == "__main__":
+    app()
